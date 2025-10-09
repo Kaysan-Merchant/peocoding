5cb2dbec5d649180730603f3acd39568b6a1b4df
Kaysan Merchant <merch.kk12@gmail.com>
2025-10-08 20:11:09 -0400

    Update - Week 1, adding button functionality

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let btm = document.getElementById('btnExample');
+let para = document.getElementById('para')
+
+btn.addEventListener('click', handleClickEvent);
+
+function handleClickEvent(){
+    para.className = 'orange';
+}
+
 console.log('hello');
